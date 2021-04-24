af8b53742df2de4858af52cb98b0a787a68e29ad
JSchrkte <jschrtke@protonmail.com>
2021-04-24 18:57:01 +0200

    Add save dialogue

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-env GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui . && env GOOS=linux GOARCH=amd64 go build .
+rm -r build
+mkdir build
+
+# build for linux
+mkdir ./build/mod2mp4-linux64 
+env GOOS=linux GOARCH=amd64 go build -o ./build/mod2mp4-linux64/mod2mp4 .
+mkdir ./build/mod2mp4-linux64/deps
+cp -r ./deps/ffmpeg-linux64/ ./build/mod2mp4-linux64/deps/ffmpeg-linux64/
+
+# build for windows
+mkdir build/mod2mp4-win64 
+env GOOS=windows GOARCH=amd64 go build -o build/mod2mp4-win64/mod2mp4.exe -ldflags -H=windowsgui . 
+mkdir ./build/mod2mp4-win64/deps
+cp -r ./deps/ffmpeg-win64/ ./build/mod2mp4-win64/deps/ffmpeg-win64/
+
+# pack everything up
+7z a build/mod2mp4-linux64.zip build/mod2mp4-linux64/*
+7z a build/mod2mp4-win64.zip build/mod2mp4-win64/*
--- main.go
+++ main.go
@@ -15,39 +15,29 @@ import (
 const outFormat = "mp4"
 
 func main() {
-	workingDir, err := os.Getwd()
+	ffmpegPath, err := getFfmpegPath()
 	if err != nil {
-		panic(err)
-	}
-
-	ffmpegPath := "ffmpeg"
-	_, err = exec.LookPath(ffmpegPath)
-	if err != nil {
-		if runtime.GOOS == "windows" {
-			ffmpegPath = filepath.Clean(filepath.Join(workingDir, "deps/ffmpeg-win64/bin/ffmpeg.exe"))
-		} else if runtime.GOOS == "linux" {
-			ffmpegPath = "deps/ffmpeg-linux64/ffmpeg"
-		} else {
-			ffmpegPath = "ffmpeg"
-		}
-		_, err := exec.LookPath(ffmpegPath)
-		if err != nil {
-			dialog.Message("Missing ffmpeg installation").Title("Error").Info()
-			return
-		}
+		dialog.Message(fmt.Sprintf("Missing ffmpeg installation: %s", err.Error())).
+			Title("Error").
+			Info()
 	}
 
 	filename, err := dialog.File().Filter("MOD video file", "MOD").Load()
-	if err != nil {
-		if err == dialog.Cancelled {
-			return
-		} else {
-			panic(err.Error())
-		}
+	if err == dialog.Cancelled {
+		return
+	} else if err != nil {
+		panic(err)
 	}
 
-	outFile := strings.TrimSuffix(filename, filepath.Ext(filename))
-	outFile = strings.Join([]string{outFile, outFormat}, ".")
+	outFile, err := dialog.File().Filter("mp4 video file", "mp4").Save()
+	if err == dialog.Cancelled {
+		return
+	} else if err != nil {
+		panic(err)
+	}
+	if !strings.HasSuffix(filepath.Ext(outFile), "mp4") {
+		outFile = strings.Join([]string{outFile, "mp4"}, ".")
+	}
 
 	err = fluentffmpeg.NewCommand(ffmpegPath).
 		InputPath(filename).
@@ -56,6 +46,30 @@ func main() {
 		Overwrite(true).
 		Run()
 	if err != nil {
-		dialog.Message(err.Error()).Title(fmt.Sprintf("error converting %s", filename)).Info()
+		dialog.Message(err.Error()).
+			Title(fmt.Sprintf("error converting %s", filename)).
+			Info()
 	}
+}
+
+func getFfmpegPath() (string, error) {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	var ffmpegPath string
+	if runtime.GOOS == "windows" {
+		ffmpegPath = filepath.Clean(filepath.Join(workingDir, "deps/ffmpeg-win64/bin/ffmpeg.exe"))
+	} else if runtime.GOOS == "linux" {
+		ffmpegPath = "deps/ffmpeg-linux64/ffmpeg"
+	} else {
+		ffmpegPath = "ffmpeg"
+	}
+	_, err = exec.LookPath(ffmpegPath)
+	if err != nil {
+		return "", err
+	}
+
+	return ffmpegPath, nil
 }
